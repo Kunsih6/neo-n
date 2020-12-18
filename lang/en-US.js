dcae1149ecf4eba89482d6b5ee5d4e0f7e9a6506
Estuardo Aguirre <EstuardoA25@gmail.com>
2020-12-18 10:35:33 -0600

    feat: error page

--- lang/en-US.js
+++ lang/en-US.js
@@ -1,4 +1,11 @@
 export default {
+  action: {
+    backToHome: 'Back to home page',
+  },
+  error: {
+    404: "Sorry, the page you're looking for doesn't exist",
+    generic: 'An error has occurred',
+  },
   meta: {
     title: 'Kunsih | Web solutions creator',
     description: 'Computer science engineer and web developer',
---
+++ layouts/error.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="error">
+    <div class="error__container">
+      <h1 class="mb-8">{{ error.statusCode }}</h1>
+      <h2 v-if="error.statusCode === 404">{{ $t('error.404') }}</h2>
+      <h2 v-else>{{ $t('error.generic') }}</h2>
+      <UtilActionButton class="mt-16">
+        <NuxtLink :to="localePath({ name: index })">
+          {{ $t('action.backToHome') }}
+        </NuxtLink>
+      </UtilActionButton>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'empty',
+  props: {
+    error: {
+      default: () => ({}),
+      required: true,
+      type: Object,
+    },
+  },
+}
+</script>
+
+<style lang="postcss" scoped>
+.error {
+  &__container {
+    @apply container flex flex-col items-center justify-center max-w-4xl min-h-screen mx-auto text-center;
+  }
+}
+</style>
